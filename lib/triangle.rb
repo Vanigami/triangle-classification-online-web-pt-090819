adab7136c2a4929002ca3fd8ef8e942cb7cf53f6
Vanna Winland <vgwinland@gmail.com>
2019-10-15 15:07:26 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,28 @@
 class Triangle
-  # write code here
+  attr_accessor :a, :b, :c
+  
+  def initialize(a, b, c)
+    @a = a 
+    @b = b 
+    @c = c 
+  end
+  
+  def kind 
+    if a == b == c 
+      :equilateral
+    elsif a == b || a == c || b == c 
+      :isosceles
+    else 
+      :scalene
+    end
+  end
+      
+    def triangle_validation
+      valid_triangle = [(a + b >c), (a + c > b), (b + c > a)]
+      valid_triangle.each {|i| valid_triangle << false if i <= 0}
+      raise TriangleError if valid_triangle.include?(false)
+    end
+    
+    class TriangleError < StandardError
+    end
 end
